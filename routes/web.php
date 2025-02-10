f11c3f1c3b0260cfd4b9c248bd84f50e4bf99d6c
Teo2Peer <teo2din@gmail.com>
2025-02-10 19:04:07 +0100

    Add route to serve index.html for non-API requests

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
+// return public folder if is diferent to /api/*, user/*,  storage/*, sanctum/*, login, register, logout. 
+Route::get('{any}', function () {
+    $path = public_path('index.html');
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    return File::get($path);
+})->where('any', '^((?!api|user|storage|sanctum).)*$');
